90c0651c6372b3c0c96ea51e8348b5a926b97bbb
artdg <gribovich.artem01@gmail.com>
2026-03-17 13:54:04 +0300

    added workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages serves the site from /<repo-name>/, not from /
+  base: '/task-list/',
   plugins: [react()],
 })
